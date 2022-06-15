866f26bc9fd36e6a070bc6058a425e471c9bc183
BiscayRobin <biscay.rob@gmail.com>
2022-06-15 20:51:06 +0200

    Basic structure for code

---
+++ sketch.js
@@ -0,0 +1,19 @@
+let width;
+let height;
+
+function windowResized() {
+    width = windowWidth;
+    height = windowHeight;
+    resizeCanvas(windowWidth, windowHeight);
+}
+
+function setup() {
+    width = windowWidth;
+    height = windowHeight;
+    createCanvas(width, height);
+    frameRate(4);
+    noStroke();
+}
+
+function draw() {
+}
